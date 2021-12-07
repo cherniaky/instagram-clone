d874a3938a45f79f663d4ad53d080c2d4f1576e6
Yurii <juracherniak@gmail.com>
2021-12-07 20:57:45 +0100

    add header and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
-//import "./styles/App.css";
+import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-    return <Router basename="/instagram-clone">
-      <div className="pagesContainer">
-        <Routes>
-          <Route exact path="/" element={}/>
-        </Routes>
-      </div>
-    </Router>;
+  const [inHome,setInHome]=useState(true);
+    return (
+        <Router basename="/instagram-clone">
+            <NavBar inHome={inHome} />
+            <div className="pagesContainer">
+                <Routes>
+                    <Route exact path="/" element={<div>hi</div>} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
